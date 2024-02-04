58b8f0979f668196e4ae6ff2806d8ec4ef88b4b8
Muhoza John <muhozajohn250@gmail.com>
2024-02-05 00:39:19 +0200

    fixing modal

--- src/components/Common/Button.jsx
+++ src/components/Common/Button.jsx
@@ -5,13 +5,22 @@ import Link from "next/link";
 const Button = ({ icon, path, title, classes }) => {
   return (
     <>
-      <Link
-        href={path}
-        className={`${classes} bg-blue-500 text-white px-6 py-3 rounded-full flex items-center justify-center gap-3 scale-95 hover:scale-100 active:scale-95 duration-100`}
-      >
-        {icon}
-        {title}
-      </Link>
+      {path ? (
+        <Link
+          href={path}
+          className={`${classes} bg-blue-500 text-white px-6 py-3 rounded-full flex items-center justify-center gap-3 scale-95 hover:scale-100 active:scale-95 duration-100`}
+        >
+          {icon}
+          {title}
+        </Link>
+      ) : (
+        <button
+          className={`${classes} bg-blue-500 text-white px-6 py-3 rounded-full flex items-center justify-center gap-3 scale-95 hover:scale-100 active:scale-95 duration-100`}
+        >
+          {icon}
+          {title}
+        </button>
+      )}
     </>
   );
 };
