44f03c80db4fcd5616f249f27f244d9c0fe5731c
Muhoza John <muhozajohn250@gmail.com>
2024-02-08 13:28:59 +0200

    changing baseUrl

--- src/app/apply/page.jsx
+++ src/app/apply/page.jsx
@@ -20,6 +20,7 @@ const ApplyPage = () => {
             <option>1</option>
             <option>2</option>
             <option>3</option>
+            <option>4</option>
           </select>
         </div>
       </div>
